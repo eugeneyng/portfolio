802c06db515c0711be5952a4e0df71b052ce9bfb
eugene <ngyeugene@gmail.com>
2022-09-15 10:27:33 -0400

    starting

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,4 +1,6 @@
 AUTHOR = 'Eugene Ng'
+DEFAULT_PAGINATION = False
+RELATIVE_URLS = True
 SITENAME = 'Portfolio'
 SITEURL = ''
 
@@ -8,24 +10,8 @@ TIMEZONE = 'America/New_York'
 
 DEFAULT_LANG = 'en'
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+# Theme
+THEME = "./pelican-portfolio"
 
-# Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
 
-DEFAULT_PAGINATION = False
-
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
